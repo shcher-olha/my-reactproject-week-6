be309968216db2ab48d63deedbd5096fd934f991
shcher-olha <shcher.olha@gmail.com>
2024-03-26 10:44:01 +0100

    instal babel plugin in package.json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <h1>React Weather-App</h1>
       <div className="container">
-        
+        <Weather defaultCity="New York" />
         <p>
           This project was coded by{" "}
-          <a
-            href="/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="/" target="_blank" rel="noreferrer">
             Shcherbak Olha
           </a>{" "}
           and is{" "}
-          <a
-            href="/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="/" target="_blank" rel="noreferrer">
             open sourced on Github
           </a>
         </p>
