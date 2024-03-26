728a486972ebfc965bd0e967bce8a5606440b303
shcher-olha <shcher.olha@gmail.com>
2024-03-26 14:41:57 +0100

    correct desine

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from './Weather';
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather-App</h1>
+      <h1 className='title'>React Weather-App</h1>
       <div className="container">
         <Weather defaultCity="New York" />
         <p>
