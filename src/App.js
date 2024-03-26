10bd4107305b236d40832d55ade649785f2905b3
shcher-olha <shcher.olha@gmail.com>
2024-03-26 10:18:29 +0100

    initial project and instal bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>React Weather-App</h1>
+      <div className="container">
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This project was coded by{" "}
+          <a
+            href="/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Shcherbak Olha
+          </a>{" "}
+          and is{" "}
+          <a
+            href="/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on Github
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
 
-export default App;
+ 
