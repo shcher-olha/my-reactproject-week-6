404e8e748304aa5ac6f634e4909b66c065fbb319
shcher-olha <shcher.olha@gmail.com>
2024-03-27 12:41:47 +0100

    last correct

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import Weather from './Weather';
 export default function App() {
   return (
     <div className="App">
-      <h1 className='title'>React Weather-App</h1>
+      <h1 className="title">React Weather-App</h1>
       <div className="container">
         <Weather defaultCity="Vaduz" />
         <p>
-          This project was coded by{" "}
-          <a href="/" target="_blank" rel="noreferrer">
+          This project was coded by 💙💛{" "}
+          <a
+            href="https://github.com/shcher-olha"
+            target="_blank"
+            rel="noreferrer"
+          >
             Shcherbak Olha
           </a>{" "}
           and is{" "}
-          <a href="/" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/shcher-olha/my-reactproject-week-6"
+            target="_blank"
+            rel="noreferrer"
+          >
             open sourced on Github
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://my-reactproject-week-6.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
           </a>
         </p>
       </div>
